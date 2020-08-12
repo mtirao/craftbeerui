abf77d422dbf2a56e8f704ba6d90ff611d051855
Marcos Tirao <mtirao@Marcoss-Mac-mini.local>
2020-08-12 18:36:46 -0300

    Bug fixing

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    fullscreen: true,
     webPreferences: {
       nodeIntegration: true
     }
@@ -14,7 +15,7 @@ function createWindow () {
   win.loadFile('src/index.html')
 
   // Open the DevTools.
-  win.webContents.openDevTools()
+  //win.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
