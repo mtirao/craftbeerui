6b5d1bda194ecd7a8e0a5700d353048a4821c69a
Marcos Tirao <mtirao@Marcoss-Mac-mini.local>
2020-08-13 17:00:24 -0300

    UI Update

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    fullscreen: true,
+    //fullscreen: true,
     webPreferences: {
       nodeIntegration: true
     }
